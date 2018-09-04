f391069cfd19c1f5743abad00d96e92d1da57230
Kirby Abadilla <kirbyabadilla@gmail.com>
2018-09-04 12:49:03 +0800

    Initial commint for the frontend implementation

---
+++ utils/meetup-api.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import axios from 'axios';
+import Auth from '../src/auth/Auth.js';
+
+const auth = new Auth();
+const BASE_URL = 'https://meetupservice2.herokuapp.com';
+
+export function getPublicMeetups() {
+  const url = `${BASE_URL}/api/meetups/public`;
+  return axios.get(url).then(response => response.data.meetups).catch(err =>  err || 'Unable to retrieve data');
+}
+
+export function getPrivateMeetups() {
+  const url = `${BASE_URL}/api/meetups/private`;
+  return axios.get(url, { headers: { Authorization: `Bearer ${localStorage.getItem('access_token')}` }}).then(response => response.data.meetups).catch(err => err || 'Unable to retrieve data');
+}
